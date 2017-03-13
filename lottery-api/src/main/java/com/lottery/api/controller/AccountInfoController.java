5b2062a47a2eecc69ea298822fa2d2ea42cb708b
Administrator <Administrator@192.168.0.35>
2017-03-13 14:10:27 +0800

    wyp

--- lottery-api/src/main/java/com/lottery/api/controller/AccountInfoController.java
+++ lottery-api/src/main/java/com/lottery/api/controller/AccountInfoController.java
@@ -130,7 +130,14 @@ public class AccountInfoController {
 		    }else {
 		    	OffAccountInfo offparaInfo = mapper.map(param, OffAccountInfo.class);
 		    	OffAccountInfo offaccountInfo = offAccountInfoMapper.selectByLogin(offparaInfo);
+		    	String query = offaccountInfo.getQuery();
 		    	if (offaccountInfo!=null){
+			    	//判断子账户
+			    	if (offaccountInfo.getOfftype().equals("2")){
+			    		offaccountInfo = offAccountInfoMapper.selectByUsername(offaccountInfo.getSupusername());
+			    		offaccountInfo.setQuery(query);
+			    	}
+		    		
 			    	AccountDetail accountDetail = accountDetailMapper.selectByUserId(offaccountInfo.getUserid(), offaccountInfo.getOfftype());
 			    	if (accountDetail == null){
 					      result.fail(MessageTool.Code_3002);
